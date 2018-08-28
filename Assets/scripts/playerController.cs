71bed21deae3425da38363cd2ccf2c9f02a34f4c
akzo <anarkazimov98@hotmail.com>
2018-08-27 20:37:06 -0700

    initial version of the test world

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        var xRotation = Input.GetAxis("Horizontal");
+        var zMovement = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, xRotation, 0);
+        transform.Translate(0, 0, zMovement);
+	}
+}
